e4bb99e8fa307d4bfb15acfdb3f4e3deec06ef54
RafaS92 <manrvaldez..92@gmail.com>
2020-03-24 18:00:11 -0500

    last first day

--- db/migrate/20200324211359_create_shoes.rb
+++ db/migrate/20200324211359_create_shoes.rb
@@ -1,7 +1,7 @@
 class CreateShoes < ActiveRecord::Migration[5.2]
   def change
     create_table(:shoes) do |t|
-      t.string(:type)
+      t.string(:category)
       t.float(:price)
       t.string(:color)
     end
